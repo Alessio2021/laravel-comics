b21ee561089aab18332ab729b1712f0eb3f1c9a7
Alessio2021 <alessiobut1911@gmail.com>
2022-02-11 17:29:45 +0100

    push

--- resources/views/guest/comics.blade.php
+++ resources/views/guest/comics.blade.php
@@ -6,6 +6,27 @@
 
 @section('content')
 
-COMICS
+    <div class="main">
+        <div class="jumbo">
+        </div>
+        <div class="container-main">
+        <h2 class="title-absolute">current series</h2>
+        <div class="container">
+            <ul class="product-list">
+                @foreach ($cards as $card)
+                    <li>
+                        <div class="container-img">
+                            <img src="{{ $card['thumb'] }}" alt="">
+                        </div>
+                        <h4>{{ $card['series'] }}</h4>
+                    </li>
+                @endforeach
+            </ul>
+            <div class="button">
+            <button>load more</button>
+            </div>
+        </div>
+        </div>
+    </div>
 
 @endsection
--- resources/views/guest/partials/header.blade.php
+++ resources/views/guest/partials/header.blade.php
@@ -1,19 +1,28 @@
-<header class="container">
-    <div class="header-sx">
-        <img src="{{asset('images/dc-logo.png')}}" alt="logo-dc">
+<header>
+    <div class="header-top">
+        <div class="container d-flex flex-end">
+            <a href="#">dc power&trade; visa&reg;</a>
+            <a href="#">additional dc sites<i class="fas fa-caret-down"></i></a>
+        </div>
     </div>
-    <div class="header-dx">
-        <ul>
-            <li><a href="{{route('characters')}}">characters</a></li>
-            <li><a href="{{route('comics')}}" href="">comics</a></li>
-            <li><a href="{{route('movies')}}">movies</a></li>
-            <li><a href="{{route('tv')}}">tv</a></li>
-            <li><a href="{{route('games')}}">games</a></li>
-            <li><a href="{{route('collectibles')}}">collectibles</a></li>
-            <li><a href="{{route('videos')}}">videos</a></li>
-            <li><a href="{{route('fans')}}">fans</a></li>
-            <li><a href="{{route('news')}}">news</a></li>
-            <li><a href="{{route('shop')}}">shop</a></li>
-        </ul>
+    <div class="container container_header">
+        <div class="header-sx">
+            <img src="{{asset('images/dc-logo.png')}}" alt="logo-dc">
+        </div>
+        <div class="header-dx">
+            <ul>
+                <li><a href="{{route('characters')}}">characters</a></li>
+                <li><a href="{{route('comics')}}" href="">comics</a></li>
+                <li><a href="{{route('movies')}}">movies</a></li>
+                <li><a href="{{route('tv')}}">tv</a></li>
+                <li><a href="{{route('games')}}">games</a></li>
+                <li><a href="{{route('collectibles')}}">collectibles</a></li>
+                <li><a href="{{route('videos')}}">videos</a></li>
+                <li><a href="{{route('fans')}}">fans</a></li>
+                <li><a href="{{route('news')}}">news</a></li>
+                <li><a href="{{route('shop')}}">shop</a></li>
+            </ul>
+            <input type="text" placeholder="Search"><i class="fas fa-search"></i>
+        </div>
     </div>
 </header>
