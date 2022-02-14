f69678f2a3e7ba9f53421c2144b8606e165c7951
Alessio2021 <alessiobut1911@gmail.com>
2022-02-14 16:09:13 +0100

    advtertising done

--- resources/views/guest/product.blade.php
+++ resources/views/guest/product.blade.php
@@ -3,9 +3,34 @@
 @section('pageTitle')
     {{$nomePagina}}
 @endsection
-    
+
 @section('content')
     
-
+    <div class="container_fluid">
+        <div class="container-product">
+            <img src="{{$product['thumb']}}" alt="$Product['title']">
+        </div>
+    </div>
+    <div class="container-product d-flex">
+        <div class="sx">
+            <h2>{{ $product['title'] }}</h2>
+            <div class="d-flex green-section">
+                <div class="w-100 price d-flex">
+                    <span class="w-60">U.S. price:<strong>{{$product['price']}}</strong></span>
+                    <span class="d-flex flex-end w-100">AVAILABLE</span>
+                </div>
+                <div class="availability w-40">
+                    <span class="">Check Availability<i class="fas fa-caret-down"></i></span>
+                </div>
+            </div>
+            <div class="description">
+                <p>{{$product['description']}}</p>
+            </div>
+        </div>
+        <div class="dx">
+            <h4>advertisement</h4>
+            <img src="{{asset('images/adv.jpg')}}" alt="">
+        </div>
+    </div>
 
 @endsection
