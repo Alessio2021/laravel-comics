96a0add9119622e38177ef3a1d59de18cead0b10
Alessio2021 <alessiobut1911@gmail.com>
2022-02-14 17:11:03 +0100

    specs

--- resources/views/guest/product.blade.php
+++ resources/views/guest/product.blade.php
@@ -32,5 +32,74 @@
             <img src="{{asset('images/adv.jpg')}}" alt="">
         </div>
     </div>
-
+    <div class="info">
+        <div class="container-product d-flex">
+            <div class="info_sx col-50">
+                <h3>Talent</h3>
+                <div class="row d-flex">
+                    <div class="col-30">
+                        <h5>Art by:</h5>
+                    </div>
+                    <div class="col">
+                        @foreach ($product as $key => $item)
+                            @if ($key === 'artists')
+                                @foreach ($item as $value)
+                                    @if ($loop->last)
+                                        <span><a href="#">{{$value}}</a></span>
+                                    @else
+                                        <span><a href="#">{{$value}}, </a></span>
+                                    @endif
+                                @endforeach
+                            @endif
+                        @endforeach
+                    </div>
+                </div>
+                <div class="row d-flex">
+                    <div class="col-30">
+                        <h5>Written by:</h5>
+                    </div>
+                    <div class="col">
+                        @foreach ($product as $key => $item)
+                            @if ($key === 'writers')
+                                @foreach ($item as $value)
+                                    @if ($loop->last)
+                                        <span><a href="#">{{$value}}</a></span>
+                                    @else
+                                        <span><a href="#">{{$value}}, </a></span>
+                                    @endif
+                                @endforeach
+                            @endif
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+            <div class="info_dx col-50">
+                <h3>Specs</h3>
+                <div class="row d-flex">
+                    <div class="col-30">
+                        <h5>Series:</h5>
+                    </div>
+                    <div class="col">
+                        <h4><a href="#">{{$product['series']}}</a></h4>
+                    </div>
+                </div>
+                <div class="row d-flex">
+                    <div class="col-30">
+                        <h5>U.S. Price:</h5>
+                    </div>
+                    <div class="col">
+                        <span>{{$product['price']}}</span>
+                    </div>
+                </div>
+                <div class="row d-flex">
+                    <div class="col-30">
+                        <h5>On Sale Date:</h5>
+                    </div>
+                    <div class="col">
+                        <span>{{ date_format(date_create($product['sale_date']),'M d Y') }}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
